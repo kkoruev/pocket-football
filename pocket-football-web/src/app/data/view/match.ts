efc5ce5d62968ce41d660fc6d4cd98204f719e43
Kristafar Koruev <k.koruev@abv.bg>
2020-09-25 18:58:26 +0300

    Added changes for invite to matches page

--- pocket-football-web/src/app/data/view/match.ts
+++ pocket-football-web/src/app/data/view/match.ts
@@ -1,4 +1,4 @@
-export interface Match {
+export class Match {
   id: string;
   title: string;
   host: string; // will check if ID or Name, or User object
@@ -6,4 +6,21 @@ export interface Match {
   currentParticipants: number;
   allowedParticipants: number;
   image: string;
+
+
+  constructor(id: string, title: string, host: string, place: string, currentParticipants: number,
+              allowedParticipants: number, image: string) {
+    this.id = id;
+    this.title = title;
+    this.host = host;
+    this.place = place;
+    this.currentParticipants = currentParticipants;
+    this.allowedParticipants = allowedParticipants;
+    this.image = image;
+  }
+
+  isMatchFull(): boolean {
+    return (this.allowedParticipants - this.currentParticipants) === 0;
+  }
+
 }
