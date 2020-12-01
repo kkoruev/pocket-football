a6867fb61436ff06580756a1fb0842091c96a64f
Kristafar Koruev <k.koruev@abv.bg>
2020-12-01 10:22:15 +0200

    Added initial register component

--- pocket-football-web/src/app/presentation/user/user.module.ts
+++ pocket-football-web/src/app/presentation/user/user.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ProfileComponent } from './profile/profile.component';
 import {MaterialModule} from '../material/material.module';
 import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 
 
 @NgModule({
-  declarations: [ProfileComponent, HomeComponent],
+  declarations: [ProfileComponent, HomeComponent, RegisterComponent],
   imports: [
     CommonModule,
     MaterialModule
