515b827a5810356977f34b85b9f0cd8045101553
Kristafar Koruev <k.koruev@abv.bg>
2020-12-03 11:37:21 +0200

    Added input fields to the register component, still in progress

--- pocket-football-web/src/app/routing/routing.module.ts
+++ pocket-football-web/src/app/routing/routing.module.ts
@@ -3,11 +3,13 @@ import {RouterModule, Routes} from '@angular/router';
 import {MyMatchComponent} from '../presentation/event/my-match/my-match.component';
 import {ProfileComponent} from '../presentation/user/profile/profile.component';
 import {HomeComponent} from '../presentation/user/home/home.component';
+import {RegisterComponent} from '../presentation/user/register/register.component';
 
 const routes: Routes = [
   {path: 'matches', component: MyMatchComponent},
   {path: 'profile', component: ProfileComponent},
-  {path: 'home', component: HomeComponent}
+  {path: 'home', component: HomeComponent},
+  {path: 'register', component: RegisterComponent}
 ];
 
 @NgModule({
